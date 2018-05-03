0fd03f797fd0ccae9bdc7b1a64531cc8a6c4135d
Ada Hagan <akhagan@umich.edu>
2018-05-03 08:36:23 -0400

    manu link + update notebook

--- XML/Code/link_xml_test.R
+++ XML/Code/link_xml_test.R
@@ -1,5 +1,4 @@
-#when manuscripts are submitted, they are assigned a unique manuscript number which follows them until they are either accepted or rejected from the system (either full rejection, or reject with resubmission). If the manuscript is then revised and resubmitted, they are assigned a new unique manuscript number and the manuscript number from the previous version is entered as a "related manuscript". This cycle can repeat multiple times until a manuscripts author either gives up or the manuscript is accepted. The problem is that only ONE "related manuscript" is provided, regardless of the number of previous iterations. In order to prevent double/triple counting authors, I need a way to link all versions of a manuscript through the process. This involves tracing a given "related manuscript" number to the version assigned that as a manuscript number, then checking if that manuscript has a "related manuscript" number to trace back, and so on and so forth. Below is my attempt at this function. Key sticking points are that 1) R doesn't like the output of my 'if()' statement 2)I can't figure out how to recycle a value within a repeat 3) am I even doing this right?
-
+library(tidyverse)
 all_manu <- read_csv("Processed_Data/all_manu.csv")#import csv 
 
 input_df <- all_manu %>% select(related.manu, manuscript.number) %>% #select only columns needed to test function (save as input_df to simplify troubleshooting my function)
@@ -10,9 +9,8 @@ link_manu <- function(input_df, input_related_manu, input_manu){
 
   repeat{#The following steps will be repeated until the if statement evaluates TRUE
     
+  if(is.na(input_related_manu)){
     revised.manu.number <- input_manu #idea is to temporarily store & rewrite revised.manu.number until the final linked manuscript has been traced
-    
-  if(input_related_manu == "NA"){
     paste(revised.manu.number);#if there is not a related manu, then paste the manuscript number in the revised.manu column
     break}
     else #if there is a related manu, then repeat the following steps
@@ -25,8 +23,10 @@ link_manu <- function(input_df, input_related_manu, input_manu){
       
       revised.manu.number <- case_when(
         y != "NA" ~ paste(";", y) %>% append(revised.manu.number, .), #if y is a true value, then paste a semi-colon in front of the value & append that true value to the end of the unique value being built
-        y == "NA" ~ paste(revised.manu.number))#if y reaches a point that it becomes NA, then re-return the same value 
+        is.na(y) ~ paste(revised.manu.number))#if y reaches a point that it becomes NA, then re-return the same value 
       }
   
   return(revised.manu.number)#populate the column with the revised.manu.number
 }
+
+#I'm only getting the manu.number back as the revised.manu.number. I think that's because it's been assigned in the beginning & isn't being re-written or the re-writing is being lost b/c of assigning the input_manu to it. So... how do I re-write it such that I don't lose the assignment? Flip it to a while statement?
